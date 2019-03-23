9a43fe92b6696a8afe0cfb586467fa1956bfdf95
hoan_g <hoan_9@users.noreply.github.com>
2019-03-23 13:56:01 +0700

    demo commit = netbeans 4. Including test file

---
+++ test/test/DemoTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import demojenkinsgit.Total;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Hoang Pham
+ */
+public class DemoTest {
+     @Test
+     public void hello() {
+         Total tmp = new Total();
+         tmp.setTotal(2, 7);
+         assertEquals(7, tmp.getTotal());
+     }
+}
